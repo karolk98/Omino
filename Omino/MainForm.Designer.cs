3894a4bf195b84e9de6d0a33d1411d4c3f3b0e5d
karolk98 <47919588+karolk98@users.noreply.github.com>
2020-11-08 23:46:13 +0100

    Added some stats

--- Omino/MainForm.Designer.cs
+++ Omino/MainForm.Designer.cs
@@ -41,29 +41,24 @@
             this.optimalSquareButton = new System.Windows.Forms.Button();
             this.generateButton = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.labelInfo1 = new System.Windows.Forms.Label();
+            this.labelInfo2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize) (this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.blockCountBox)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.blockSizeBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.blockCountBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.blockSizeBox)).BeginInit();
             this.SuspendLayout();
-            // 
-            // splitContainer1
-            // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
             this.splitContainer1.Name = "splitContainer1";
-            // 
-            // splitContainer1.Panel1
-            // 
             this.splitContainer1.Panel1.AutoScroll = true;
             this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.splitContainer1.Panel1.Controls.Add(this.pictureBox);
-            // 
-            // splitContainer1.Panel2
-            // 
+            this.splitContainer1.Panel2.Controls.Add(this.labelInfo2);
+            this.splitContainer1.Panel2.Controls.Add(this.labelInfo1);
             this.splitContainer1.Panel2.Controls.Add(this.blockCountBox);
             this.splitContainer1.Panel2.Controls.Add(this.blockSizeBox);
             this.splitContainer1.Panel2.Controls.Add(this.label2);
@@ -77,9 +72,6 @@
             this.splitContainer1.Size = new System.Drawing.Size(800, 450);
             this.splitContainer1.SplitterDistance = 635;
             this.splitContainer1.TabIndex = 0;
-            // 
-            // pictureBox
-            // 
             this.pictureBox.BackColor = System.Drawing.Color.White;
             this.pictureBox.Location = new System.Drawing.Point(3, 3);
             this.pictureBox.Name = "pictureBox";
@@ -87,51 +79,28 @@
             this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox.TabIndex = 0;
             this.pictureBox.TabStop = false;
-            // 
-            // blockCountBox
-            // 
             this.blockCountBox.Location = new System.Drawing.Point(28, 125);
             this.blockCountBox.Name = "blockCountBox";
             this.blockCountBox.Size = new System.Drawing.Size(120, 20);
             this.blockCountBox.TabIndex = 11;
-            this.blockCountBox.Value = new decimal(new int[] {
-            5,
-            0,
-            0,
-            0});
-            // 
-            // blockSizeBox
-            // 
+            this.blockCountBox.Value = new decimal(new int[] {5, 0, 0, 0});
             this.blockSizeBox.Location = new System.Drawing.Point(28, 86);
             this.blockSizeBox.Name = "blockSizeBox";
             this.blockSizeBox.Size = new System.Drawing.Size(120, 20);
             this.blockSizeBox.TabIndex = 10;
-            this.blockSizeBox.Value = new decimal(new int[] {
-            6,
-            0,
-            0,
-            0});
-            // 
-            // label2
-            // 
+            this.blockSizeBox.Value = new decimal(new int[] {6, 0, 0, 0});
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(25, 109);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(70, 13);
             this.label2.TabIndex = 9;
             this.label2.Text = "Blocks Count";
-            // 
-            // label1
-            // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(25, 70);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(57, 13);
             this.label1.TabIndex = 8;
             this.label1.Text = "Block Size";
-            // 
-            // heuristicRectangleButton
-            // 
             this.heuristicRectangleButton.Location = new System.Drawing.Point(14, 405);
             this.heuristicRectangleButton.Name = "heuristicRectangleButton";
             this.heuristicRectangleButton.Size = new System.Drawing.Size(134, 23);
@@ -139,9 +108,6 @@
             this.heuristicRectangleButton.Text = "Heuristic Rectangle";
             this.heuristicRectangleButton.UseVisualStyleBackColor = true;
             this.heuristicRectangleButton.Click += new System.EventHandler(this.heuristicRectangleButton_Click);
-            // 
-            // optimalRectangleButton
-            // 
             this.optimalRectangleButton.Location = new System.Drawing.Point(15, 376);
             this.optimalRectangleButton.Name = "optimalRectangleButton";
             this.optimalRectangleButton.Size = new System.Drawing.Size(134, 23);
@@ -149,9 +115,6 @@
             this.optimalRectangleButton.Text = "Optimal Rectangle";
             this.optimalRectangleButton.UseVisualStyleBackColor = true;
             this.optimalRectangleButton.Click += new System.EventHandler(this.optimalRectangleButton_Click);
-            // 
-            // heuristicSquareButton
-            // 
             this.heuristicSquareButton.Location = new System.Drawing.Point(14, 347);
             this.heuristicSquareButton.Name = "heuristicSquareButton";
             this.heuristicSquareButton.Size = new System.Drawing.Size(134, 23);
@@ -159,9 +122,6 @@
             this.heuristicSquareButton.Text = "Heuristic Square";
             this.heuristicSquareButton.UseVisualStyleBackColor = true;
             this.heuristicSquareButton.Click += new System.EventHandler(this.heuristicSquareButton_Click);
-            // 
-            // optimalSquareButton
-            // 
             this.optimalSquareButton.Location = new System.Drawing.Point(14, 318);
             this.optimalSquareButton.Name = "optimalSquareButton";
             this.optimalSquareButton.Size = new System.Drawing.Size(134, 23);
@@ -169,9 +129,6 @@
             this.optimalSquareButton.Text = "Optimal Square";
             this.optimalSquareButton.UseVisualStyleBackColor = true;
             this.optimalSquareButton.Click += new System.EventHandler(this.optimalSquareButton_Click);
-            // 
-            // generateButton
-            // 
             this.generateButton.Location = new System.Drawing.Point(42, 150);
             this.generateButton.Name = "generateButton";
             this.generateButton.Size = new System.Drawing.Size(75, 23);
@@ -179,18 +136,22 @@
             this.generateButton.Text = "Generate";
             this.generateButton.UseVisualStyleBackColor = true;
             this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
-            // 
-            // button1
-            // 
             this.button1.Location = new System.Drawing.Point(41, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "From File";
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // MainForm
-            // 
+            this.labelInfo1.AutoSize = true;
+            this.labelInfo1.Location = new System.Drawing.Point(60, 227);
+            this.labelInfo1.Name = "labelInfo1";
+            this.labelInfo1.Size = new System.Drawing.Size(0, 13);
+            this.labelInfo1.TabIndex = 12;
+            this.labelInfo2.AutoSize = true;
+            this.labelInfo2.Location = new System.Drawing.Point(60, 256);
+            this.labelInfo2.Name = "labelInfo2";
+            this.labelInfo2.Size = new System.Drawing.Size(0, 13);
+            this.labelInfo2.TabIndex = 13;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
@@ -202,13 +163,12 @@
             this.splitContainer1.Panel1.PerformLayout();
             this.splitContainer1.Panel2.ResumeLayout(false);
             this.splitContainer1.Panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.blockCountBox)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.blockSizeBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.blockCountBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.blockSizeBox)).EndInit();
             this.ResumeLayout(false);
-
         }
 
         #endregion
@@ -225,5 +185,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.NumericUpDown blockCountBox;
         private System.Windows.Forms.NumericUpDown blockSizeBox;
+        private System.Windows.Forms.Label labelInfo2;
+        private System.Windows.Forms.Label labelInfo1;
     }
 }
--- Omino/MainForm.cs
+++ Omino/MainForm.cs
@@ -57,60 +57,84 @@ namespace Omino
         private void optimalSquareButton_Click(object sender, System.EventArgs e)
         {
             CancelRunningTask();
+            if (board.Blocks == null) return;
+            var watch = new System.Diagnostics.Stopwatch();
             int[,] result = new int[0,0];
             Task task = Task.Factory.StartNew(() =>
             {
+                watch.Start();
                 result = board.Square();
+                watch.Stop();
             }, _tokenSource.Token).ContinueWith(_ => {
                 var bitmap = BitmapGenerator.DrawSolution(result);
 
                 pictureBox.ClientSize = new Size(bitmap.Width, bitmap.Height);
                 pictureBox.Image = bitmap;
+                labelInfo1.Text = $"Time: {watch.ElapsedMilliseconds} ms";
+                labelInfo2.Text = $"Square size: {result.GetLength(0)}x{result.GetLength(0)}";
             }, _tokenSource.Token, TaskContinuationOptions.None, TaskScheduler.FromCurrentSynchronizationContext());
         }
 
         private void heuristicSquareButton_Click(object sender, System.EventArgs e)
         {
             CancelRunningTask();
+            if (board.Blocks == null) return;
+            var watch = new System.Diagnostics.Stopwatch();
             int[,] result = new int[0, 0];
             Task task = Task.Factory.StartNew(() =>
             {
+                watch.Start();
                 result = board.FastSquare();
+                watch.Stop();
             }, _tokenSource.Token).ContinueWith(_ => {
                 var bitmap = BitmapGenerator.DrawSolution(result);
 
                 pictureBox.ClientSize = new Size(bitmap.Width, bitmap.Height);
                 pictureBox.Image = bitmap;
+                labelInfo1.Text = $"Time: {watch.ElapsedMilliseconds} ms";
+                labelInfo2.Text = $"Square size: {result.GetLength(0)}x{result.GetLength(0)}";
             }, TaskScheduler.FromCurrentSynchronizationContext());
         }
 
         private void optimalRectangleButton_Click(object sender, System.EventArgs e)
         {
             CancelRunningTask();
+            if (board.Blocks == null) return;
+            var watch = new System.Diagnostics.Stopwatch();
             Tuple<int[,], int> result = new Tuple<int[,], int>(new int[0, 0], 0);
             Task task = Task.Factory.StartNew(() =>
             {
+                watch.Start();
                 result = board.Rectangle();
+                watch.Stop();
             }, _tokenSource.Token).ContinueWith(_ => {
                 var bitmap = BitmapGenerator.DrawSolution(result.Item1);
 
                 pictureBox.ClientSize = new Size(bitmap.Width, bitmap.Height);
                 pictureBox.Image = bitmap;
+                labelInfo1.Text = $"Time: {watch.ElapsedMilliseconds} ms";
+                labelInfo2.Text = $"Cuts: {result.Item2}";
             }, TaskScheduler.FromCurrentSynchronizationContext());
         }
 
         private void heuristicRectangleButton_Click(object sender, System.EventArgs e)
         {
             CancelRunningTask();
+            if (board.Blocks == null) return;
+            var watch = new System.Diagnostics.Stopwatch();
             Tuple<int[,], int> result = new Tuple<int[,], int>(new int[0, 0], 0);
             Task task = Task.Factory.StartNew(() =>
             {
+                watch.Start();
                 result = board.FastRectangle();
+                watch.Stop();
             }, _tokenSource.Token).ContinueWith(_ => {
                 var bitmap = BitmapGenerator.DrawSolution(result.Item1);
 
                 pictureBox.ClientSize = new Size(bitmap.Width, bitmap.Height);
                 pictureBox.Image = bitmap;
+                labelInfo1.Text = $"Time: {watch.ElapsedMilliseconds} ms";
+                labelInfo2.Text = $"Cuts: {result.Item2}";
             }, TaskScheduler.FromCurrentSynchronizationContext());
         }
 
@@ -118,6 +142,8 @@ namespace Omino
         {
             _tokenSource.Cancel();
             _tokenSource = new CancellationTokenSource();
+            labelInfo1.Text = "";
+            labelInfo2.Text = "";
         }
     }
 }
